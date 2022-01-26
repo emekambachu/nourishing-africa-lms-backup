1ba43a45274346d59886c0a05f4a9b38a4249240
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-01-26 23:29:16 +0100

    update

--- resources/views/yaedp/account/modules/course.blade.php
+++ resources/views/yaedp/account/modules/course.blade.php
@@ -22,15 +22,24 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="course-video">
-                                <iframe width="853" height="480" src="https://web.microsoftstream.com/embed/video/161c45c5-56d4-4dcf-903e-0251679f7f7e?autoplay=false&showinfo=true" allowfullscreen style="border:none;"></iframe>
+                                <!--For responsiveness-->
+                                <div style='max-width: 853px'>
+                                    <div style='position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;'>
+                                        <!--Insert Iframe-->
+                                        <iframe width="853" height="480" src="https://web.microsoftstream.com/embed/video/161c45c5-56d4-4dcf-903e-0251679f7f7e?autoplay=false&showinfo=true" allowfullscreen style="border:none; position: absolute; top: 0; left: 0; right: 0; bottom: 0; height: 100%; max-width: 100%;"></iframe>
+
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-12">
-                            <span>Description</span>
-                            <span>Instructor</span>
-                            <span>Resources</span>
-                            <span>Discussion</span>
+
+                        <div class="col-12 mt-5">
+                            <div class="d-inline text-dark course-tabs">Description</div>
+                            <div class="d-inline text-dark course-tabs">Instructor</div>
+                            <div class="d-inline text-dark course-tabs">Resources</div>
+                            <div class="d-inline text-dark course-tabs">Discussion</div>
                         </div>
+
                     </div>
                 </div>
             </div>
