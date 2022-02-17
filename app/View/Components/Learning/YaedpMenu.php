c78f2bc9bd219cd5548519f2696b58a87e7cee1d
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-02-17 10:30:15 +0100

    update

--- app/View/Components/Learning/YaedpMenu.php
+++ app/View/Components/Learning/YaedpMenu.php
@@ -6,7 +6,7 @@ use App\Models\Learning\LearningCategory;
 use App\Models\Learning\LearningModule;
 use Illuminate\View\Component;
 
-class yaedpMenu extends Component
+class YaedpMenu extends Component
 {
     /**
      * Create a new component instance.
@@ -29,6 +29,6 @@ class yaedpMenu extends Component
         $data['modules'] = LearningModule::where('learning_category_id', $data['categoryId'])
             ->orderBy('created_at', 'asc')->get();
 
-        return view('components.learning.yaedp-menu', $data);
+        return view('yaedp.account.components.yaedp-menu', $data);
     }
 }
--- resources/views/yaedp/account/includes/layout.blade.php
+++ resources/views/yaedp/account/includes/layout.blade.php
@@ -104,7 +104,7 @@
                     <i class="angle fe fe-chevron-down"></i>
                 </a>
 
-                <x-Learning.yaedpMenu/>
+                <x-Learning.YaedpMenu/>
             </li>
 
             <li class="slide mb-3">
