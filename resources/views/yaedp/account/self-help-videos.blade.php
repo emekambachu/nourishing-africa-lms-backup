b53d54215321ca8dccbcd59097d91a1ecf4faf94
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-05-05 11:14:37 +0100

    update

--- resources/views/yaedp/account/self-help-videos.blade.php
+++ resources/views/yaedp/account/self-help-videos.blade.php
@@ -21,17 +21,10 @@
         </div>
 
         <div class="row">
-            <div class="col-md-12">
-                <h5 class="na-text-dark-green text-manrope tx-md-bold">
-                    WATCH HELPFUL HOW-TO VIDEOS FOR PARTICIPANTS</h5>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-6">
+            <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4 class="na-text-dark-green">Update your profile</h4>
+                        <h5 class="na-text-dark-green">Update your profile</h5>
                     </div>
                     <div class="card-body text-inter shadow-2">
                         <iframe src="https://drive.google.com/file/d/1zC1LiiTtPwQ-zT5CVJffUvE4YZsIeFex/preview"
@@ -40,10 +33,10 @@
                 </div>
             </div>
 
-            <div class="col-md-6">
+            <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4 class="na-text-dark-green">Starting a journey</h4>
+                        <h5 class="na-text-dark-green">Starting a journey</h5>
                     </div>
                     <div class="card-body text-inter shadow-2">
                         <iframe src="https://drive.google.com/file/d/1wykcNaz8pU3hnoadHi4Pv3oDnDnt2cSo/preview"
@@ -51,6 +44,43 @@
                     </div>
                 </div>
             </div>
+
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="na-text-dark-green">Taking an assessment</h5>
+                    </div>
+                    <div class="card-body text-inter shadow-2">
+                        <iframe src="https://drive.google.com/file/d/1uRHNYP0jZyIcSsyzuiyAlxf8lCPYtJay/preview"
+                                width="100%" height="480" allow="autoplay"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="na-text-dark-green">Viewing your assessment</h5>
+                    </div>
+                    <div class="card-body text-inter shadow-2">
+                        <iframe src="https://drive.google.com/file/d/1WzPgWPbKmFoTZN8TXvvnTOzPDUQsGPr-/preview"
+                                width="100%" height="480" allow="autoplay"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="na-text-dark-green">Downloading your certificate</h5>
+                    </div>
+                    <div class="card-body text-inter shadow-2">
+                        <iframe src="https://drive.google.com/file/d/1HQFby-aLDf3e062IFiXSusCaTwdIFthd/preview"
+                                width="640" height="480" allow="autoplay"></iframe>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
         <div class="row">
