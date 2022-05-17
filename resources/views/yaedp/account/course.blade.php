01f2766138c49d54be7996766610b818fefc73b3
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-05-17 13:55:38 +0100

    update

--- resources/views/yaedp/account/course.blade.php
+++ resources/views/yaedp/account/course.blade.php
@@ -68,7 +68,7 @@
                             </div>
                             <div class="col-md-10 col-sm-12">
                                 <h4 class="text-inter text-dark mb-0">{{ $course->trainer }}</h4>
-                                <p class="text-inter text-grey tx-14">{!! $course->trainer_bio !!}</p>
+                                <p class="text-inter text-grey tx-14 text-justify">{!! $course->trainer_bio !!}</p>
                             </div>
                         </div>
                     </div>
