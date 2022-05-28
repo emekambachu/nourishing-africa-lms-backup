5fe34aa90fa73b63af761c4cd39df9bf16c1c339
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-05-28 12:42:57 +0100

    added right click prevention script

--- resources/views/yaedp/account/includes/layout.blade.php
+++ resources/views/yaedp/account/includes/layout.blade.php
@@ -65,7 +65,9 @@
 
 </head>
 
-<body class="main-body app sidebar-mini" style="background-color: #f9fafd;">
+<body class="main-body app sidebar-mini"
+      style="background-color: #f9fafd;"
+      oncontextmenu="return false;">
 
 <!-- Loader -->
 <div id="global-loader">
