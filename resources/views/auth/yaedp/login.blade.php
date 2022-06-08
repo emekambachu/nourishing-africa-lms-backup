7ebfbcb698aba4ca938b20ba2f292b1cef99b353
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-06-08 12:43:14 +0100

    update

--- resources/views/auth/yaedp/login.blade.php
+++ resources/views/auth/yaedp/login.blade.php
@@ -35,89 +35,83 @@
         <div class="container-fluid">
             <div class="row justify-content-center">
 
-                <div class="col-11 col-md-10 margin-50px-tb padding-four-all md-padding-eight-all md-padding-15px-lr sm-padding-50px-tb border-radius-4 card"
+                <div class="col-11 col-md-6 margin-50px-tb padding-four-all md-padding-eight-all md-padding-15px-lr sm-padding-50px-tb border-radius-4 card"
                      style="visibility: visible; animation-name: fadeInLeft; background-color: #fff;">
                     <div class="row m-0">
                         <div class="col-12 margin-six-bottom lg-margin-six-bottom md-margin-30px-bottom sm-no-margin-bottom">
-                            <h4 class="na-text-dark-green learning-intro-header text-center mx-auto mx-lg-0 sm-width-100 mb-0">Welcome to YAEDP</h4>
-                            <p class="text-center font-normal-manrope-black">
-                                We have been notified that some of you are experiencing difficulties accessing the YAEDP e-learning portal. We appreciate your emails and messages sent to inform us.<br><br>
-                                Our team is now working on the backend to optimize the portal's responsiveness and efficiency. We expect that the issue will be resolved within the next few days and all participants will be able to access the portal without errors.<br><br>
-                                Please be rest assured that we will amend the training dates accordingly to ensure that you still have the dedicated six weeks to complete the training.<br>
-                                We will duly notify you via the existing communications channels as soon as the optimization process is complete.<br>
-                                We appreciate your support and apologize for any inconvenience caused.
-                            </p>
-
+                            <h4 class="na-text-dark-green learning-intro-header text-center mx-auto mx-lg-0 sm-width-100 mb-0">
+                                Welcome to YAEDP</h4>
+                            <p class="text-center font-normal-manrope-black">Login to start learning</p>
                             @include('includes.alerts')
                         </div>
                         <!-- start feature box item -->
-{{--                        <div class="col-12 aligncenter margin-six-bottom md-margin-30px-bottom last-paragraph-no-margin">--}}
+                        <div class="col-12 aligncenter margin-six-bottom md-margin-30px-bottom last-paragraph-no-margin">
 
-{{--                            <form method="post" action="{{ route('yaedp.login.submit') }}">--}}
-{{--                                @csrf--}}
-{{--                                <div class="row">--}}
-{{--                                    <div class="col-12">--}}
-{{--                                        <div class="form-group">--}}
-{{--                                            <label class="login-form-label">Email</label><br>--}}
-{{--                                            <input type="email" name="email"--}}
-{{--                                                   class="login-form @error('email') is-invalid @enderror"--}}
-{{--                                                   placeholder="email" required="">--}}
-{{--                                            @error('email')--}}
-{{--                                                <span class="invalid-feedback" role="alert">--}}
-{{--                                                    <strong>{{ $message }}</strong>--}}
-{{--                                                </span>--}}
-{{--                                            @enderror--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
+                            <form method="post" action="{{ route('yaedp.login.submit') }}">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="form-group">
+                                            <label class="login-form-label">Email</label><br>
+                                            <input type="email" name="email"
+                                                   class="login-form @error('email') is-invalid @enderror"
+                                                   placeholder="email" required="">
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
 
-{{--                                    <div class="col-md-12 col-xl-12 col-lg-12">--}}
-{{--                                        <div class="form-group">--}}
-{{--                                            <label class="login-form-label">Password</label><br>--}}
-{{--                                            <input type="password" name="password" id="password"--}}
-{{--                                                   class="login-form @error('password') is-invalid @enderror"--}}
-{{--                                                   placeholder="Enter Password" required="">--}}
-{{--                                            <i class="far fa-eye field-icon" id="toggle_password"></i>--}}
-{{--                                            @error('password')--}}
-{{--                                                <span class="invalid-feedback" role="alert">--}}
-{{--                                                    <strong>{{ $message }}</strong>--}}
-{{--                                                </span>--}}
-{{--                                            @enderror--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
+                                    <div class="col-md-12 col-xl-12 col-lg-12">
+                                        <div class="form-group">
+                                            <label class="login-form-label">Password</label><br>
+                                            <input type="password" name="password" id="password"
+                                                   class="login-form @error('password') is-invalid @enderror"
+                                                   placeholder="Enter Password" required="">
+                                            <i class="far fa-eye field-icon" id="toggle_password"></i>
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
 
-{{--                                    <div class="col-md-6 col-lg-6 col-xs-6">--}}
-{{--                                        <div class="float-left" style="display: block;">--}}
-{{--                                            <input class="login-form-checkbox"--}}
-{{--                                                   style="display: inline; width: 20px;" type="checkbox"--}}
-{{--                                                   name="remember" id="remember">--}}
-{{--                                            <label class="text-small">Remember me</label>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
+                                    <div class="col-md-6 col-lg-6 col-xs-6">
+                                        <div class="float-left" style="display: block;">
+                                            <input class="login-form-checkbox"
+                                                   style="display: inline; width: 20px;" type="checkbox"
+                                                   name="remember" id="remember">
+                                            <label class="text-small">Remember me</label>
+                                        </div>
+                                    </div>
 
-{{--                                    <div class="col-md-6 col-lg-6 col-xs-6">--}}
-{{--                                        <div class="float-right" style="display: block;">--}}
-{{--                                            <p class="text-center login-shortcut">--}}
-{{--                                                <a class="na-text-dark-green"--}}
-{{--                                                   href="{{ route('yaedp.forgot-password') }}">--}}
-{{--                                                    Reset Password</a>--}}
-{{--                                            </p>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
+                                    <div class="col-md-6 col-lg-6 col-xs-6">
+                                        <div class="float-right" style="display: block;">
+                                            <p class="text-center login-shortcut">
+                                            <a class="na-text-dark-green"
+                                               href="{{ route('yaedp.forgot-password') }}">
+                                                Reset Password</a>
+                                            </p>
+                                        </div>
+                                    </div>
 
-{{--                                    <div class="col-12">--}}
-{{--                                        <button type="submit" class="login-form-btn">Login</button>--}}
-{{--                                    </div>--}}
+                                    <div class="col-12">
+                                        <button type="submit" class="login-form-btn">Login</button>
+                                    </div>
 
-{{--                                    <div class="col-12">--}}
-{{--                                        <p class="font-small-manrope-black margin-10px-bottom">--}}
-{{--                                            By using this you agree to our <a class="login-link"--}}
-{{--                                                  href="{{ route('yaedp.terms') }}">Terms of Use</a>--}}
-{{--                                        </p>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
+                                    <div class="col-12">
+                                        <p class="font-small-manrope-black margin-10px-bottom">
+                                            By using this you agree to our <a class="login-link"
+                                                  href="{{ route('yaedp.terms') }}">Terms of Use</a>
+                                        </p>
+                                    </div>
+                                </div>
 
-{{--                            </form>--}}
-{{--                        </div>--}}
+                            </form>
+                        </div>
                         <!-- end feature box item -->
                     </div>
                 </div>
@@ -129,30 +123,30 @@
 
 @section('bottom-assets')
     <script src="{{ asset('learning-assets/custom/js/login-form.js') }}"></script>
-{{--    <script>--}}
-{{--    $(function() {--}}
-{{--        $(document).ready(function () {--}}
-{{--            document.cookie = 'my_cookie=; path=/; domain=.nourishingafrica.com; expires=' + new Date(0).toUTCString();--}}
-{{--            (function () {--}}
-{{--                let cookies = document.cookie.split("; ");--}}
-{{--                for (let c = 0; c < cookies.length; c++) {--}}
-{{--                    let d = window.location.hostname.split(".");--}}
-{{--                    while (d.length > 0) {--}}
-{{--                        let cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + 'nourishingafrica.com' + ' ;path=';--}}
-{{--                        console.log(d[1] + '.' + d[2]);--}}
-{{--                        let p = location.pathname.split('/');--}}
-{{--                        document.cookie = cookieBase + '/';--}}
-{{--                        while (p.length > 0) {--}}
-{{--                            document.cookie = cookieBase + p.join('/');--}}
-{{--                            p.pop();--}}
-{{--                        }--}}
-{{--                        d.shift();--}}
-{{--                    }--}}
-{{--                }--}}
-{{--                console.log('cookie deleted');--}}
-{{--                console.log(window.location.hostname);--}}
-{{--            })();--}}
-{{--        });--}}
-{{--    });--}}
-{{--    </script>--}}
+    {{--    <script>--}}
+    {{--    $(function() {--}}
+    {{--        $(document).ready(function () {--}}
+    {{--            document.cookie = 'my_cookie=; path=/; domain=.nourishingafrica.com; expires=' + new Date(0).toUTCString();--}}
+    {{--            (function () {--}}
+    {{--                let cookies = document.cookie.split("; ");--}}
+    {{--                for (let c = 0; c < cookies.length; c++) {--}}
+    {{--                    let d = window.location.hostname.split(".");--}}
+    {{--                    while (d.length > 0) {--}}
+    {{--                        let cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + 'nourishingafrica.com' + ' ;path=';--}}
+    {{--                        console.log(d[1] + '.' + d[2]);--}}
+    {{--                        let p = location.pathname.split('/');--}}
+    {{--                        document.cookie = cookieBase + '/';--}}
+    {{--                        while (p.length > 0) {--}}
+    {{--                            document.cookie = cookieBase + p.join('/');--}}
+    {{--                            p.pop();--}}
+    {{--                        }--}}
+    {{--                        d.shift();--}}
+    {{--                    }--}}
+    {{--                }--}}
+    {{--                console.log('cookie deleted');--}}
+    {{--                console.log(window.location.hostname);--}}
+    {{--            })();--}}
+    {{--        });--}}
+    {{--    });--}}
+    {{--    </script>--}}
 @endsection
