a9286b23744b2075d72bdfbf32d0361bf72613b9
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-06-09 13:11:28 +0100

    updated password reset functionality

---
+++ app/Http/Requests/Account/PasswordResetRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests\Account;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PasswordResetRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email|min:4',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'Email is required!',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        if($this->wantsJson()){
+            $response = response()->json([
+                "success" => false,
+                "errors" => $validator->getMessageBag()->toArray()
+            ]);
+        }
+
+//        if($this->wantsJson()) {
+//            $response = response()->json([
+//                'success' => false,
+//                'message' => 'Ops! Some errors occurred',
+//                'errors' => $validator->errors()
+//            ]);
+//        }else{
+//            $response = redirect()
+//                ->route('guest.login')
+//                ->with('message', 'Ops! Some errors occurred')
+//                ->withErrors($validator);
+//        }
+
+        throw (new ValidationException($validator, $response))
+            ->errorBag($this->errorBag)
+            ->redirectTo($this->getRedirectUrl());
+    }
+}
---
+++ app/Services/Learning/Account/PasswordResetService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services\Learning\Account;
+
+use App\Models\YaedpUser;
+use Illuminate\Support\Facades\Mail;
+
+/**
+ * Class PasswordResetService.
+ */
+class PasswordResetService
+{
+    public static function verificationToken($length = 12){
+        $characters = '0123456789ABCDEFGHI';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    public static function processPasswordReset($request){
+
+        $input = $request->all();
+        $userExists = YaedpUser::where([
+            ['email', $input['email']]
+        ])->first();
+
+        if(!$userExists){
+            return response()->json([
+                'success' => false,
+                'message' => 'Email does not exist',
+            ]);
+        }
+
+        $userExists->verification_token = self::verificationToken();
+        $userExists->save();
+
+        return $userExists;
+    }
+
+    public static function sendPasswordResetEmail($user){
+
+        // data for email
+        $data = [
+            'name' => $user->name,
+            'email' => $user->email,
+            'verification_token' => $user->verification_token,
+        ];
+
+        // Send email to the NA Team
+        Mail::send('emails.yaedp.password-reset-link', $data, static function ($message) use ($data) {
+            $message->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME').' | Password Reset');
+            $message->to($data['email']);
+            $message->replyTo(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME').' | Password Reset');
+            $message->subject('YAEDP Password Reset Link');
+        });
+
+        return $data;
+    }
+}
