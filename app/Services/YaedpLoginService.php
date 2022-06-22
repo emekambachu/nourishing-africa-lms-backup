c9a83dc61d0b019639682de301b31200b75eedae
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-06-22 08:10:16 +0100

    update

--- app/Services/YaedpLoginService.php
+++ app/Services/YaedpLoginService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Models\Learning\LearningLoginSession;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Class YaedpLoginService.
@@ -18,16 +19,19 @@ class YaedpLoginService extends BaseService
 
         $hasSession = BaseService::learningLoginSession()
             ->where('email', $request->email)->first();
+        $userId = BaseService::yaedpUser()
+            ->where('email', $request->email)->first()->id;
 
         if($hasSession){
             $hasSession->update([
-                'user_id' => $hasSession->id,
+                'user_id' => $userId,
                 'ip' => $ip,
                 'user_agent' => $user_agent,
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]);
         }else{
             BaseService::learningLoginSession()->create([
+                'user_id' => $userId,
                 'learning_category_id' => 3,
                 'email' => $request->email,
                 'ip' => $ip,
