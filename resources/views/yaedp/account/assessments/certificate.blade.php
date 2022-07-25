524d5722b7b32c43c80ae40f031639387805125b
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-07-25 10:04:48 +0100

    update certificate status

--- resources/views/yaedp/account/assessments/certificate.blade.php
+++ resources/views/yaedp/account/assessments/certificate.blade.php
@@ -19,35 +19,37 @@
             <h2 class="font-large-inter text-light-brown font-weight-bold">Certificate</h2>
         </div>
 
-        @if($completedAssessment && $completedAssessment->percent > 79)
-{{--            <div class="row" style="margin-bottom: 100px;">--}}
-{{--                <div class="certificate-bg-sample">--}}
-{{--                    <img src="{{ asset('images/icons/certificate_yaedp_500_sample.png') }}"/>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-
-            <div class="row justify-content-center">
-                <div class="col-12 text-center">
-                    <h4><strong>Congratulations</strong> Congratulations on completing your modules!
-                        We will inform you via email when your certificate is ready for download.</h4>
+        @if($completedAssessment)
+            @if($completedAssessment->percent > 79)
+                <div class="row justify-content-center">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="{{ asset('images/icons/certification.png') }}" width="250"/>
+                    </div>
+                    <div class="col-12 text-center">
+                        <h4>
+                            <strong class="text-success">Congratulations</strong> on completing your modules!
+                            We will inform you via email when your certificate is ready for download.
+                        </h4>
+                    </div>
                 </div>
-            </div>
-
-{{--            <div class="certificate-bg justify-content-center">--}}
-{{--                <h4 class="certificate-name">{{ Auth::user()->first_name.' '.Auth::user()->surname }}</h4>--}}
-{{--                <p class="certificate-date">{{ $certificateDate }}</p>--}}
-{{--            </div>--}}
-{{--            <div class="col-12 justify-content-center">--}}
-{{--                <a href="{{ route('yaedp.account.assessment.certificate.download') }}">--}}
-{{--                    <button style="width:150px;" class="module-btn bg-light-brown d-flex justify-content-center">--}}
-{{--                        Download</button>--}}
-{{--                </a>--}}
-{{--            </div>--}}
+            @else
+                <div class="row justify-content-center">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="{{ asset('images/icons/fail-test.png') }}" width="250"/>
+                    </div>
+                    <div class="col-12 text-center">
+                        <h4>
+                            Unfortunately, You haven't met the cut-off mark.<br>
+                            Try to re-take certain module assessments and aim for a higher score.
+                        </h4>
+                    </div>
+                </div>
+            @endif
         @else
             <div class="row justify-content-center">
                 <div class="col-12 text-center">
-                    <p>You are yet to complete your modules.<br>
-                        Please complete all outstanding courses and assessments to view your certificate.</p>
+                    <h4>You are yet to complete your modules.<br>
+                        Please complete all outstanding courses and assessments to view your certificate.</h4>
                 </div>
             </div>
             <div class="row">
