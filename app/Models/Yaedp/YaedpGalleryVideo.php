6a5ff2f486f3b84af2d12c7854e467f4f27d9e3e
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-12-10 17:09:33 +0000

    working on gallery images and videos

---
+++ app/Models/Yaedp/YaedpGalleryVideo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Yaedp;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class YaedpGalleryVideo extends Model
+{
+    use HasFactory;
+}
